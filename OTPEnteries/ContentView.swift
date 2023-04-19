488a9438cef87536780dc465890a0f3585844865
Swee Kwang <sweekwang94@gmail.com>
2023-04-19 20:21:29 +0800

    Initial Commit

---
+++ OTPEnteries/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OTPEnteries
+//
+//  Created by Swee Kwang Chua on 19/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OTPEnteries/OTPEnteriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTPEnteriesApp.swift
+//  OTPEnteries
+//
+//  Created by Swee Kwang Chua on 19/4/23.
+//
+
+import SwiftUI
+
+@main
+struct OTPEnteriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
